d7de9d3721a5729ec85edf1b1ec6d77bbbf5c0c3
Hlobile <62103682+Hlobile@users.noreply.github.com>
2021-05-18 22:45:33 +0200

    Added-Method

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <q-page padding >
     <input v-model="message" /><!--bind v-model to message-->
     <!--add event button next to input,-->
-    <button @click= "message = ''">Clear</button>
+    <button @click= "clearMessage">Clear</button>
     <h5>{{message}}</h5>
   </q-page>
 </template>
@@ -16,6 +16,12 @@ export default{
    data(){
      return{
        message: "I love God"
+     }
+   },
+
+   methods: {
+     clearMessage(){
+       this.message = ''
      }
    }
 
