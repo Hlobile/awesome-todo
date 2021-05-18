ff36caed7fc58c5a9d29ed7a57bdffd8d6d8de9d
Hlobile <62103682+Hlobile@users.noreply.github.com>
2021-05-18 22:43:04 +0200

    install quasar elements

---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page padding >
+    <input v-model="message" /><!--bind v-model to message-->
+    <!--add event button next to input,-->
+    <button @click= "message = ''">Clear</button>
+    <h5>{{message}}</h5>
+  </q-page>
+</template>
+
+<script lang="ts">
+
+import { Vue, Component } from 'vue-property-decorator';
+
+
+export default{
+   data(){
+     return{
+       message: "I love God"
+     }
+   }
+
+
+}
+  
+
+</script>
